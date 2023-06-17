b11837282c5b5eabae4b041f07f0deaa876797de
Lyabin Andrey <lyabinovich47@yandex.ru>
2023-06-17 12:41:22 +0300

    поправил адрес подключения к базе на 127.0.0.1

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const {
   PORT = 3000,
-  MONGO_URL = 'mongodb://0.0.0.0:27017/mestodb',
+  MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb',
   NODE_ENV,
   JWT_SECRET,
 } = process.env;
