db6f2215b5ac01e35074589e2a3647305b53c9da
Lyabin Andrey <lyabinovich47@yandex.ru>
2023-06-20 22:14:49 +0300

    еще раз поправил cors

--- backend/app.js
+++ backend/app.js
@@ -13,12 +13,13 @@ const { PORT, MONGO_URL } = require('./config');
 mongoose.connect(MONGO_URL, {});
 
 const app = express();
-// app.use(cors({ origin: ['http://localhost:3001', 'https://lyabinovich47.nomoredomains.rocks'] }));
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:3001', 'https://lyabinovich47.nomoredomains.rocks'] }));
+// app.use(cors());
 
 // app.use(express.static(path.join(__dirname, 'public'))); убираем по ревью, т.к. нет папки
 app.use(requestLogger); // логгер запросов
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // GET /crash-test — тест сервера на востановление
 // не забыть удалить этот код после прохождения ревью
